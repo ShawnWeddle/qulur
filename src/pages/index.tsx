a23d7580bfce5463fc36080cd17e53d1bdfb40bb
unknown <marlinzeah@gmail.com>
2023-06-11 18:22:25 -0400

    import alias change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import MainLogo from "~/components/mainLogo";
+import MainLogo from "../components/mainLogo";
 import { useAuthContext } from "~/hooks/useAuthContext";
 
 const Home: NextPage = () => {
